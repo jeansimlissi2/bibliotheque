bed558a018ed9e847d3539f4cc1d6017408b47c7
jeansimlissi2 <jeansimlissi2@gmail.com>
2026-01-06 23:41:35 +0000

    auto save

--- bibliotheque/admin/connexion.php
+++ bibliotheque/admin/connexion.php
@@ -1,11 +1,11 @@
 <?php
-$host = getenv('MYSQL_HOST') ?: 'o84o4skkk8000c0kkcsos4oc';
+_oc';
 $user = getenv('MYSQL_USER') ?: 'root';
 $pass = getenv('MYSQL_PASSWORD') ?: 'lXYwcOFPIZkMp3oS6vNycUj1IwQw2nDm2tdg9fW68oB4qCZJECqdT1jxkEQWornj';
-$dbname = getenv('MYSQL_DATABASE') ?: 'default';
-$port = getenv('MYSQL_PORT') ?: '3306';
+$dbname = getenv('MYSQL_DATABASE') ?: 'bibliotheque';
+$port = (int)(getenv('MYSQL_PORT') ?: 3306);
 
-$con = new mysqli($host . ':' . $port, $user, $pass, $dbname);
+$con = new mysqli($host, $user, $pass, $dbname, $port);
 
 if ($con->connect_error) {
     error_log("Database connection failed: " . $con->connect_error);
